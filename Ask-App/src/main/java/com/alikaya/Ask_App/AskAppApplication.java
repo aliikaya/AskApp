172652382235ef2f87d31c5dd73ff63f26365820
Ali Kaya <alikaya@mail.com>
2024-09-20 15:13:28 +0300

    first commit

---
+++ Ask-App/src/main/java/com/alikaya/Ask_App/AskAppApplication.java
@@ -0,0 +1,13 @@
+package com.alikaya.Ask_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskAppApplication.class, args);
+	}
+
+}
