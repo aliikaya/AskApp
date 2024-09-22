f02a87b788aac43727aba47cf7d226e4f0459062
Ali Kaya <alikaya@mail.com>
2024-09-22 22:59:29 +0300

    Add get post delete metods

--- Ask-App/src/main/java/com/alikaya/Ask_App/controllers/PostController.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/controllers/PostController.java
@@ -3,14 +3,20 @@ package com.alikaya.Ask_App.controllers;
 import java.util.List;
 
 import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alikaya.Ask_App.entities.Post;
+import com.alikaya.Ask_App.request.PostCreateRequest;
+import com.alikaya.Ask_App.request.PostUpdateRequest;
 import com.alikaya.Ask_App.services.PostService;
 
 @RestController
@@ -29,13 +35,27 @@ public class PostController {
     }
 
     @PostMapping
-    public Post createOnePost(@RequestBody Post newPost){
-        return postService.saveOnePost(newPost);
+    public Post createOnePost(@RequestBody PostCreateRequest newPostRequest){
+        return postService.createOnePost(newPostRequest);
     }
 
-    @GetMapping("{/userId}")
-    public Post getOnePost(@RequestBody Long userId){
-        return postService.getOnePost(userId);
+    @GetMapping("/{postId}")
+    public Post getOnePost(@PathVariable Long postId){
+        return postService.getOnePostById(postId);
     }
+
+    @PutMapping("/{postId}")
+    public Post updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequest updatePost){
+        return postService.updatePost(postId, updatePost);
+    }
+    
+    @DeleteMapping("/{postId}")
+    public void deleteOnePost(@PathVariable Long postId){
+         postService.deleteOnePost(postId);
+
+    }
+    
+
+   
 
 }
