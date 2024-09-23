5b655789f4039657c26b7454ef03953f4e8a9388
Ali Kaya <alikaya@mail.com>
2024-09-23 16:10:09 +0300

    Creted CommentService.java

---
+++ Ask-App/src/main/java/com/alikaya/Ask_App/services/CommentService.java
@@ -0,0 +1,72 @@
+package com.alikaya.Ask_App.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.alikaya.Ask_App.entities.Comment;
+import com.alikaya.Ask_App.entities.Post;
+import com.alikaya.Ask_App.entities.User;
+import com.alikaya.Ask_App.repos.CommentRepository;
+import com.alikaya.Ask_App.request.CommentCreateRequest;
+import com.alikaya.Ask_App.request.CommentUpdateRequest;
+
+@Service
+public class CommentService {
+    private CommentRepository commentRepository;
+    private UserService userService;
+    private PostService postService;
+
+
+    public CommentService(CommentRepository commentRepository,UserService userService, PostService postService){
+        this.commentRepository = commentRepository;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    public List<Comment> getAllCommentsWithParam(Optional<Long> postId, Optional<Long> userId) {
+        if(postId.isPresent() && userId.isPresent()){
+            return commentRepository.findByUserIdAndPostId(userId.get(),postId.get());
+        }else if(userId.isPresent()){
+            return commentRepository.findByUserId(userId.get());
+        }else if(postId.isPresent()){
+            return commentRepository.findByPostId(postId.get());
+        }else{
+            return commentRepository.findAll();
+        }
+    }
+
+    public Comment getOneComment(Long commentId) {
+        return commentRepository.findById(commentId).orElse(null);
+    }
+
+    public Comment createOneComment(CommentCreateRequest newCommentRequest) {
+        User user = userService.getOneUserById(newCommentRequest.getUserId());
+        Post post = postService.getOnePostById(newCommentRequest.getPostId());
+        if(user != null && post != null){
+            Comment commentToSave = new Comment();
+            commentToSave.setId(newCommentRequest.getId());
+            commentToSave.setText(newCommentRequest.getText());
+            commentToSave.setPost(post);
+            commentToSave.setUser(user);
+            return commentRepository.save(commentToSave);
+        }else 
+            return null;
+    }
+
+    public Comment UpdateOneCommentById(Long commentId, CommentUpdateRequest updateComment) {
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        if(comment.isPresent()){
+            Comment toUpdate = comment.get();
+            toUpdate.setText(updateComment.getText());
+            return commentRepository.save(toUpdate);
+        }else
+            return null;
+    }
+
+    public void deleteOneCommentById(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
+
+}
