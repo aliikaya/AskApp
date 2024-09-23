60899ec67be46fc72723a18ae469af2825b13160
Ali Kaya <alikaya@mail.com>
2024-09-23 16:06:36 +0300

    Added methods to Comment repository for querying by userId and postId

--- Ask-App/src/main/java/com/alikaya/Ask_App/repos/CommentRepository.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/repos/CommentRepository.java
@@ -1,9 +1,21 @@
 package com.alikaya.Ask_App.repos;
 
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alikaya.Ask_App.entities.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>{
+
+    List<Comment> findByUserIdAndPostId(Long userId, Long postId);
+
+    List<Comment> findByUserId(Long userId);
+
+    List<Comment> findByPostId(Long postId);
+
+    
 
 }
