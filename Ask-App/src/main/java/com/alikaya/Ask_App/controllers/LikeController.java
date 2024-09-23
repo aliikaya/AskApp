c56c120fba63a2ddbedf6986e1335c282b8892ff
Ali Kaya <alikaya@mail.com>
2024-09-23 16:02:11 +0300

    Created LikeController

---
+++ Ask-App/src/main/java/com/alikaya/Ask_App/controllers/LikeController.java
@@ -0,0 +1,53 @@
+package com.alikaya.Ask_App.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+
+import com.alikaya.Ask_App.entities.Like;
+import com.alikaya.Ask_App.request.LikeCreateRequest;
+import com.alikaya.Ask_App.services.LikeService;
+
+@RestController
+@RequestMapping("/{likes}")
+public class LikeController {
+
+    private LikeService likeService;
+
+    public LikeController(LikeService likeService){
+        this.likeService = likeService;
+    }
+
+    @GetMapping
+    public List<Like> getAllLikes(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId){
+        return likeService.getAllLikesWithParam(userId, postId);
+    }
+
+    @GetMapping("/{likeId}")
+    public Like getOneLike(@PathVariable Long likeId){
+        return likeService.getOneLikeById(likeId);
+    }
+
+    @PostMapping
+    public Like CreateOneLike(@RequestBody LikeCreateRequest newLike){
+        return likeService.createOneLike(newLike);
+    }
+
+    @DeleteMapping("/{likeId}")
+    public void deleteOneLike(@PathVariable Long likeId){
+        likeService.deleteOneLike(likeId);
+    }
+
+    
+
+
+}
