04740f087d29d89d014bf1cc0a13133fa68d4641
Ali Kaya <alikaya@mail.com>
2024-09-23 16:03:01 +0300

    Updated methods name

--- Ask-App/src/main/java/com/alikaya/Ask_App/controllers/UserController.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/controllers/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
     @GetMapping("/{userId}")
     public User getOneUser(@PathVariable Long userId) {
         // custom exception ekle
-        return userService.getOneUser(userId);
+        return userService.getOneUserById(userId);
     }
 
     @PutMapping("/{userId}")
