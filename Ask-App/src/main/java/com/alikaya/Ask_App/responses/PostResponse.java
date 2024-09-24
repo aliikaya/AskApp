ae989738bbb2b77bb81da9ecb0ddca4255a33c4e
Ali Kaya <alikaya@mail.com>
2024-09-24 21:04:42 +0300

    Create PostResponse class to encapsulate post data for API responses

---
+++ Ask-App/src/main/java/com/alikaya/Ask_App/responses/PostResponse.java
@@ -0,0 +1,27 @@
+package com.alikaya.Ask_App.responses;
+
+import com.alikaya.Ask_App.entities.Post;
+
+import lombok.Data;
+
+@Data
+public class PostResponse {
+    Long id;
+    Long userId;
+    String userName;
+    String title;
+    String text;
+
+    public PostResponse(Post entity){
+
+        this.id = entity.getId();
+        this.userId = entity.getUser().getId();
+        this.userName = entity.getUser().getUsername();
+        this.title = entity.getTitle();
+        this.text = entity.getText();
+        
+
+    }
+
+
+}
