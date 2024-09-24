e9948b8db3a5f489981768e8505ed8c3b71f7de0
Ali Kaya <alikaya@mail.com>
2024-09-24 21:00:33 +0300

    Refactor Navbar to use Material-UI components for a modern look

--- askapp-front/src/components/Navbar/Navbar.js
+++ askapp-front/src/components/Navbar/Navbar.js
@@ -1,18 +1,46 @@
- import React from "react";
- import{Link} from "react-router-dom";
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
- function Navbar() {
-    let userId = 5;
-    return(
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to={{pathname : '/users/' + userId}}>User</Link></li>
-            </ul>
-        </div>
-    )
+import React from "react";
+import { Link } from "react-router-dom";
 
- }
+function Navbar() {
+  let userId = 5;
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: '#1B7F79' }}>
+          <Toolbar>
+            {/* Sol kısım: Home ve MenuIcon */}
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/"
+              sx={{ color: "inherit", textDecoration: 'none' }}
+            >
+              Home
+            </Typography>
 
+            <Box sx={{ flexGrow: 1 }} />
 
- export default Navbar;
+            
+            <Button color="inherit" component={Link} to={`/users/${userId}`}>
+              User
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+}
+
+export default Navbar;
