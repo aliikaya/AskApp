9c8c06e1864e8681f09c91cb3063fcf1a5029889
Ali Kaya <alikaya@mail.com>
2024-09-26 22:42:30 +0300

    Enhance Post component to load and display comments dynamically

--- askapp-front/src/components/post/post.js
+++ askapp-front/src/components/post/post.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -7,11 +7,12 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import GradeIcon from '@mui/icons-material/Grade';
+import GradeIcon from "@mui/icons-material/Grade";
 import Collapse from "@mui/material/Collapse";
-import CommentIcon from '@mui/icons-material/Comment';
+import CommentIcon from "@mui/icons-material/Comment";
 import { Link } from "react-router-dom";
+import { Container } from "@mui/material";
+import Comment from "../Comment/Comment";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -28,35 +29,67 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
 }));
 
 function Post(props) {
-  const { title, text, userName, userId } = props;
+  const { title, text, userId, userName, postId } = props;
   const [expanded, setExpanded] = useState(false);
-  const[ liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [commentList, setCommentList] = useState([]);
+  const [error, setError] = useState(null);
+  const isInitialMount = useRef(true);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
+    refreshComments();
+    console.log(commentList);
   };
   const handleLike = () => {
     setLiked(!liked);
-  }
+  };
+
+  const refreshComments = () => {
+    fetch("/comments?postId=" + postId)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setCommentList(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
+
+  useEffect(() => {
+    if (isInitialMount.current) isInitialMount.current = false;
+    else refreshComments();
+  }, []);
 
   return (
     <div className="postContainer">
-      <Card sx={{ width: 800 }}>
+      <Card sx={{ width: 800, margin: 3 }}>
         <CardHeader
           avatar={
-
-            <Link to={`/users/${userId}`} style={{ textDecoration: 'none' }}>
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            <Link to={`/users/${userId}`} style={{ textDecoration: "none" }}>
+              <Avatar
+                sx={{
+                  bgcolor: "#E6F0E8", // Butonun arka plan rengine uyumlu
+                  color: "#1B7F79", // Yazı rengini koyu yeşil tonlarına çekelim
+                  width: 40, // Avatarın genişliğini ayarladık
+                  height: 40, // Avatarın yüksekliğini ayarladık
+                  fontWeight: "bold", // Kalın yazı
+                  fontSize: "16px", // Yazı boyutunu küçülttük
+                  border: "2px solid #E1CAB3", // Butonun hover rengini kenar çizgisi olarak ekledik
+                }}
+                aria-label="recipe"
+              >
                 {userName.charAt(0).toUpperCase()}
               </Avatar>
             </Link>
-
-
           }
-
           title={title}
           titleTypographyProps={{ align: "left" }}
-
         />
 
         <CardContent>
@@ -65,12 +98,8 @@ function Post(props) {
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites"
-            onClick={handleLike}
-          >
-            
+          <IconButton aria-label="add to favorites" onClick={handleLike}>
             <GradeIcon style={liked ? { color: "#FF4858" } : null} />
-
           </IconButton>
 
           <ExpandMore
@@ -79,18 +108,22 @@ function Post(props) {
             aria-expanded={expanded}
             aria-label="show more"
           >
-            <CommentIcon sx={{ color: '#1B7F79' }} />
+            <CommentIcon sx={{ color: "#1B7F79" }} />
           </ExpandMore>
         </CardActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent >
-            {/* Ekstra içerik buraya gelebilir */}
-            Extra content here.
-          </CardContent>
+          <Container fixed>
+            {error? "error"
+              : isLoaded
+              ? commentList.map((comment) => (
+                  <Comment userId={1} userName={"USER"} text={comment.text} />
+                ))
+              : "loading"}
+          </Container>
         </Collapse>
       </Card>
-      {title}
-      {text}
+      {/* {title}
+      {text} */}
     </div>
   );
 }
