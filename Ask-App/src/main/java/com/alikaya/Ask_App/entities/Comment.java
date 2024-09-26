8cf66b33e3db884d21e0e3e757b485e7514a4677
Ali Kaya <alikaya@mail.com>
2024-09-26 22:33:28 +0300

    Refactor entity classes with ID generation strategy

--- Ask-App/src/main/java/com/alikaya/Ask_App/entities/Comment.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/entities/Comment.java
@@ -8,9 +8,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -20,6 +21,7 @@ import lombok.Data;
 @Data
 public class Comment {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)                                   
     Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -34,7 +36,7 @@ public class Comment {
     @JsonIgnore
     User user;
 
-    @Lob
+        
     @Column(columnDefinition = "text")
     String text;
 
--- Ask-App/src/main/java/com/alikaya/Ask_App/entities/Post.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/entities/Post.java
@@ -3,11 +3,11 @@ package com.alikaya.Ask_App.entities;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,7 +19,8 @@ import lombok.Data;
 @Data
 public class Post {                      
 
-    @Id                                    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)                                   
     Long id;
 
     //bunları not al
