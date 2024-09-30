2758b07c0e276e5adff70ec2a35fd2bb9d960735
Ali Kaya <alikaya@mail.com>
2024-09-30 20:43:06 +0300

    Added postLike field to PostResponse to include likes for each post

--- Ask-App/src/main/java/com/alikaya/Ask_App/responses/PostResponse.java
+++ Ask-App/src/main/java/com/alikaya/Ask_App/responses/PostResponse.java
@@ -1,5 +1,7 @@
 package com.alikaya.Ask_App.responses;
 
+import java.util.List;
+
 import com.alikaya.Ask_App.entities.Post;
 
 import lombok.Data;
@@ -11,14 +13,16 @@ public class PostResponse {
     String userName;
     String title;
     String text;
+    List<LikeResponse> postLike;
 
-    public PostResponse(Post entity){
+    public PostResponse(Post entity, List<LikeResponse> likes){
 
         this.id = entity.getId();
         this.userId = entity.getUser().getId();
         this.userName = entity.getUser().getUsername();
         this.title = entity.getTitle();
         this.text = entity.getText();
+        this.postLike = likes;
         
 
     }
